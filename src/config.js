4c9e10f7c3336abaa546666f450d4a0c3b556bef
rutvikpumak <rutvikumak@gmail.com>
2023-10-11 14:05:13 +0530

    client id change

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const oktaConfig = {
 	issuer: `https://dev-75155220.okta.com/oauth2/default`,
-	clientId: '0oacdl3nqu1058x065d7',
+	clientId: '0oacof2ynwRHfSnDN5d7',
 	redirectUri: `${window.location.origin}/login/callback`,
 	scopes: ["openid", "profile", "email"],
 	pkce: true,
